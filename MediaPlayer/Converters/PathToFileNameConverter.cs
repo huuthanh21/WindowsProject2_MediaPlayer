f08a987c96d1e953f71ff65ed4d0ae6a676089ba
Thanh Tran <newacc2312@gmail.com>
2023-01-14 20:21:06 +0700

    Implement Playing Queue, Open file, files, folder

---
+++ MediaPlayer/Converters/PathToFileNameConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Windows.Data;
+
+namespace MediaPlayer.Converters
+{
+    public class PathToFileNameConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Path.GetFileName(value as string);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- MediaPlayer/MainWindow.xaml.cs
+++ MediaPlayer/MainWindow.xaml.cs
@@ -1,6 +1,12 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using Ookii.Dialogs.Wpf;
+using System;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.IO;
+using System.Linq;
 using System.Windows;
+using System.Windows.Media;
 using System.Windows.Threading;
 
 namespace MediaPlayerApp
@@ -11,25 +17,26 @@ namespace MediaPlayerApp
     public partial class MainWindow : Window, INotifyPropertyChanged
     {
         public bool IsPlaying { get; set; } = false;
-
         public TimeSpan MediaTimeSpan { get; set; }
         private DispatcherTimer _timer;
+        private readonly ObservableCollection<string> _currentPlaylist = new();
 
         public MainWindow()
         {
             InitializeComponent();
+
+            QueueMedia.ItemsSource = _currentPlaylist;
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
+            this.KeyDown += MainWindow_KeyDown;
             // Update MediaState when media ended on its own
             MainMediaPlayer.MediaEnded += (sender, eventArgs) =>
             {
-                MainMediaPlayer.Stop();
-                IsPlaying = false;
-                SeekBar.Value = 0;
+                Stop();
             };
 
             MainMediaPlayer.Play();
@@ -40,6 +47,22 @@ namespace MediaPlayerApp
                 Interval = new TimeSpan(0, 0, 0, 0, 500)
             };
             _timer.Tick += Timer_Tick;
+
+            // Set default volume for media
+            MainMediaPlayer.Volume = 0.7;
+        }
+
+        private void MainWindow_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if (e.Key == System.Windows.Input.Key.Space)
+            {
+                if (IsPlaying)
+                {
+                    Pause();
+                    return;
+                }
+                Play();
+            }
         }
 
         private void Play()
@@ -47,6 +70,8 @@ namespace MediaPlayerApp
             IsPlaying = true;
             MainMediaPlayer.Play();
             _timer.Start();
+
+            ButtonPlay.Content = this.Resources["icon_pause"] as DrawingImage;
         }
 
         private void Pause()
@@ -54,6 +79,8 @@ namespace MediaPlayerApp
             IsPlaying = false;
             MainMediaPlayer.Pause();
             _timer.Stop();
+
+            ButtonPlay.Content = this.Resources["icon_play"] as DrawingImage;
         }
 
         private void Stop()
@@ -63,31 +90,53 @@ namespace MediaPlayerApp
             _timer.Stop();
             SeekBar.Value = 0;
             TextblockCurrentTimestamp.Text = "00:00:00";
+
+            ButtonPlay.Content = this.Resources["icon_play"] as DrawingImage;
+        }
+
+        private enum SkipOption
+        {
+            SkipForward,
+            SkipBackward
+        }
+
+        private void Skip(SkipOption option)
+        {
+            if (MainMediaPlayer.Source == null)
+                return;
+
+            var path = MainMediaPlayer.Source.AbsolutePath;
+            path = Path.GetFullPath(Uri.UnescapeDataString(path));
+
+            int index = _currentPlaylist.IndexOf(path);
+            int newIndex = 0;
+            if (option == SkipOption.SkipForward && index + 1 != _currentPlaylist.Count)
+            {
+                newIndex = index + 1;
+            }
+            else if (option == SkipOption.SkipBackward && index > 0)
+            {
+                newIndex = index - 1;
+            }
+
+            PlayPath(_currentPlaylist[newIndex]);
         }
 
         private void Timer_Tick(object sender, EventArgs e)
         {
             var position = MainMediaPlayer.Position;
+            SeekBar.Value = position.TotalSeconds;
             TextblockCurrentTimestamp.Text = string.Format("{0:00}:{1:00}:{2:00}", position.Hours, position.Minutes, position.Seconds);
         }
 
         private void ButtonPlay_Click(object sender, RoutedEventArgs e)
         {
-            if (!IsPlaying) Play();
-        }
-
-        private void ButtonPause_Click(object sender, RoutedEventArgs e)
-        {
-            switch (IsPlaying)
+            if (IsPlaying)
             {
-                case true:
-                    Pause();
-                    break;
-
-                default:
-                    Play();
-                    break;
+                Pause();
+                return;
             }
+            Play();
         }
 
         private void ButtonStop_Click(object sender, RoutedEventArgs e)
@@ -100,7 +149,6 @@ namespace MediaPlayerApp
             double value = SeekBar.Value;
             var newPosition = TimeSpan.FromSeconds(value);
             MainMediaPlayer.Position = newPosition;
-            Timer_Tick(sender, e);
         }
 
         private void MainMediaPlayer_MediaOpened(object sender, RoutedEventArgs e)
@@ -115,7 +163,7 @@ namespace MediaPlayerApp
             // Use this instead of Pause() so that when drag ended we can use IsPlaying to determine
             // if the video should be continued to play
             MainMediaPlayer.Pause();
-            MainMediaPlayer.Volume = 0;
+            MainMediaPlayer.IsMuted = true;
         }
 
         private void SeekBar_DragCompleted(object sender, System.Windows.Controls.Primitives.DragCompletedEventArgs e)
@@ -125,7 +173,153 @@ namespace MediaPlayerApp
             {
                 MainMediaPlayer.Play();
             }
-            MainMediaPlayer.Volume = 0.5;
+            MainMediaPlayer.IsMuted = false;
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            MainMediaPlayer.Volume = SliderVolume.Value;
+
+            if (SliderVolume.Value == 0)
+            {
+                MainMediaPlayer.IsMuted = true;
+                ButtonVolume.Content = this.Resources["icon_volume_off"] as DrawingImage;
+            }
+            else
+            {
+                MainMediaPlayer.IsMuted = false;
+                ButtonVolume.Content = this.Resources["icon_volume"] as DrawingImage;
+            }
+        }
+
+        private void ButtonVolume_Click(object sender, RoutedEventArgs e)
+        {
+            MainMediaPlayer.IsMuted = !MainMediaPlayer.IsMuted;
+            if (MainMediaPlayer.IsMuted)
+            {
+                ButtonVolume.Content = this.Resources["icon_volume_off"] as DrawingImage;
+            }
+            else
+            {
+                ButtonVolume.Content = this.Resources["icon_volume"] as DrawingImage;
+            }
+        }
+
+        private void ButtonExit_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void ButtonOpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new OpenFileDialog
+            {
+                Filter = "Video files (*.mp4, *.wmv, *.avi)|*.mp4;*.wmv;*.avi|Audio files (*.mp3, *.wav)|*.mp3;*.wav",
+                InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyMusic)
+            };
+
+            if (dialog.ShowDialog() == true)
+            {
+                _currentPlaylist.Clear();
+                _currentPlaylist.Add(dialog.FileName);
+                Stop();
+                string filePath = dialog.FileName;
+
+                PlayPath(filePath);
+            }
+        }
+
+        private void PlayPath(string filePath)
+        {
+            QueueMedia.SelectedIndex = _currentPlaylist.IndexOf(filePath);
+            MainMediaPlayer.Source = new Uri(filePath, UriKind.Absolute);
+            Play();
+
+            _timer = new DispatcherTimer
+            {
+                Interval = new TimeSpan(0, 0, 0, 1, 0)
+            };
+
+            _timer.Tick += Timer_Tick;
+        }
+
+        private void ButtonOpenFiles_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new OpenFileDialog
+            {
+                Filter = "Video files (*.mp4, *.wmv, *.avi)|*.mp4;*.wmv;*.avi|Audio files (*.mp3, *.wav)|*.mp3;*.wav",
+                InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyMusic),
+                Multiselect = true
+            };
+
+            if (dialog.ShowDialog() == true)
+            {
+                Stop();
+                _currentPlaylist.Clear();
+                foreach (var name in dialog.FileNames)
+                {
+                    _currentPlaylist.Add(name);
+                }
+                string filePath = _currentPlaylist[0];
+                PlayPath(filePath);
+            }
+        }
+
+        private void ButtonOpenFolder_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new VistaFolderBrowserDialog();
+
+            if (dialog.ShowDialog() == true)
+            {
+                Stop();
+                _currentPlaylist.Clear();
+
+                // Get media files in the folder
+                var directory = new DirectoryInfo(dialog.SelectedPath);
+                var mediaFiles = directory.GetFiles()
+                                          .Where(f => f.Extension is ".mp4" or ".wmv" or ".avi" or ".mp3" or ".wav")
+                                          .ToArray();
+
+                foreach (var mediaFile in mediaFiles)
+                {
+                    _currentPlaylist.Add(mediaFile.FullName);
+                }
+
+                string filePath = _currentPlaylist[0];
+                PlayPath(filePath);
+            }
+        }
+
+        private void ButtonShowQueue_Click(object sender, RoutedEventArgs e)
+        {
+            if (QueueMedia.Visibility == Visibility.Visible)
+            {
+                QueueMedia.Visibility = Visibility.Collapsed;
+                QueueColumn.Width = new GridLength(0);
+                return;
+            }
+
+            QueueMedia.Visibility = Visibility.Visible;
+            QueueColumn.Width = new GridLength(160);
+        }
+
+        private void ButtonSkipBackward_Click(object sender, RoutedEventArgs e)
+        {
+            Skip(SkipOption.SkipBackward);
+        }
+
+        private void ButtonSkipForward_Click(object sender, RoutedEventArgs e)
+        {
+            Skip(SkipOption.SkipForward);
+        }
+
+        private void QueueMedia_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            var file = QueueMedia.SelectedItem as string;
+            if (file is not null)
+            {
+                PlayPath(file);
+            }
         }
     }
 }
