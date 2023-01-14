3141538e7692c4003aed21674d236e781a611932
Thanh Tran <newacc2312@gmail.com>
2023-01-14 22:50:15 +0700

    Implement Add to queue, Remove from queue

--- MediaPlayer/MainWindow.xaml.cs
+++ MediaPlayer/MainWindow.xaml.cs
@@ -22,13 +22,14 @@ namespace MediaPlayerApp
         public bool IsPlaying { get; set; } = false;
         public TimeSpan MediaTimeSpan { get; set; }
         private DispatcherTimer _timer;
-        private readonly ObservableCollection<string> _currentPlaylist = new();
+        private string _currentPlaylist;
+        private readonly ObservableCollection<string> _queue = new();
 
         public MainWindow()
         {
             InitializeComponent();
 
-            QueueMedia.ItemsSource = _currentPlaylist;
+            QueueMedia.ItemsSource = _queue;
         }
 
 #pragma warning disable 67
@@ -45,7 +46,14 @@ namespace MediaPlayerApp
             // Update MediaState when media ended on its own
             MainMediaPlayer.MediaEnded += (sender, eventArgs) =>
             {
-                Stop();
+                if (_queue.Count > 0)
+                {
+                    Skip(SkipOption.Forward);
+                }
+                else
+                {
+                    Stop();
+                }
             };
 
             MainMediaPlayer.Play();
@@ -105,30 +113,37 @@ namespace MediaPlayerApp
 
         private enum SkipOption
         {
-            SkipForward,
-            SkipBackward
+            Forward,
+            Backward
         }
 
         private void Skip(SkipOption option)
         {
-            if (MainMediaPlayer.Source == null)
+            if (MainMediaPlayer.Source is null)
                 return;
+            if (_queue.Count == 0)
+            {
+                MainMediaPlayer.Source = null;
+                MainMediaPlayer.Play();
+                Stop();
+                return;
+            }
 
             var path = MainMediaPlayer.Source.AbsolutePath;
             path = Path.GetFullPath(Uri.UnescapeDataString(path));
 
-            int index = _currentPlaylist.IndexOf(path);
+            int index = _queue.IndexOf(path);
             int newIndex = 0;
-            if (option == SkipOption.SkipForward && index + 1 != _currentPlaylist.Count)
+            if (option == SkipOption.Forward && index + 1 != _queue.Count)
             {
                 newIndex = index + 1;
             }
-            else if (option == SkipOption.SkipBackward && index > 0)
+            else if (option == SkipOption.Backward && index > 0)
             {
                 newIndex = index - 1;
             }
 
-            PlayPath(_currentPlaylist[newIndex]);
+            PlayPath(_queue[newIndex]);
         }
 
         private void Timer_Tick(object sender, EventArgs e)
@@ -183,13 +198,15 @@ namespace MediaPlayerApp
 
                 newMenuItem.Click += (s, e) =>
                 {
-                    _currentPlaylist.Clear();
+                    _queue.Clear();
 
                     string[] paths = File.ReadAllLines(playlistItem.FullName);
                     foreach (string path in paths)
                     {
-                        _currentPlaylist.Add(path);
+                        _queue.Add(path);
                     }
+                    _currentPlaylist = (string)newMenuItem.Header;
+                    PlayPath(_queue[0]);
                 };
 
                 ButtonOpenPlaylist.Items.Add(newMenuItem);
@@ -253,23 +270,24 @@ namespace MediaPlayerApp
             var dialog = new OpenFileDialog
             {
                 Filter = "Video files (*.mp4, *.wmv, *.avi)|*.mp4;*.wmv;*.avi|Audio files (*.mp3, *.wav)|*.mp3;*.wav",
-                InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyMusic)
+                InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyVideos)
             };
 
             if (dialog.ShowDialog() == true)
             {
-                _currentPlaylist.Clear();
-                _currentPlaylist.Add(dialog.FileName);
+                _queue.Clear();
+                _queue.Add(dialog.FileName);
                 Stop();
-                string filePath = dialog.FileName;
 
+                _currentPlaylist = null;
+                string filePath = dialog.FileName;
                 PlayPath(filePath);
             }
         }
 
         private void PlayPath(string filePath)
         {
-            QueueMedia.SelectedIndex = _currentPlaylist.IndexOf(filePath);
+            QueueMedia.SelectedIndex = _queue.IndexOf(filePath);
             MainMediaPlayer.Source = new Uri(filePath, UriKind.Absolute);
             Play();
 
@@ -286,19 +304,21 @@ namespace MediaPlayerApp
             var dialog = new OpenFileDialog
             {
                 Filter = "Video files (*.mp4, *.wmv, *.avi)|*.mp4;*.wmv;*.avi|Audio files (*.mp3, *.wav)|*.mp3;*.wav",
-                InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyMusic),
+                InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyVideos),
                 Multiselect = true
             };
 
             if (dialog.ShowDialog() == true)
             {
                 Stop();
-                _currentPlaylist.Clear();
+                _queue.Clear();
                 foreach (var name in dialog.FileNames)
                 {
-                    _currentPlaylist.Add(name);
+                    _queue.Add(name);
                 }
-                string filePath = _currentPlaylist[0];
+
+                _currentPlaylist = null;
+                string filePath = _queue[0];
                 PlayPath(filePath);
             }
         }
@@ -310,7 +330,7 @@ namespace MediaPlayerApp
             if (dialog.ShowDialog() == true)
             {
                 Stop();
-                _currentPlaylist.Clear();
+                _queue.Clear();
 
                 // Get media files in the folder
                 var directory = new DirectoryInfo(dialog.SelectedPath);
@@ -320,10 +340,11 @@ namespace MediaPlayerApp
 
                 foreach (var mediaFile in mediaFiles)
                 {
-                    _currentPlaylist.Add(mediaFile.FullName);
+                    _queue.Add(mediaFile.FullName);
                 }
 
-                string filePath = _currentPlaylist[0];
+                _currentPlaylist = null;
+                string filePath = _queue[0];
                 PlayPath(filePath);
             }
         }
@@ -343,12 +364,12 @@ namespace MediaPlayerApp
 
         private void ButtonSkipBackward_Click(object sender, RoutedEventArgs e)
         {
-            Skip(SkipOption.SkipBackward);
+            Skip(SkipOption.Backward);
         }
 
         private void ButtonSkipForward_Click(object sender, RoutedEventArgs e)
         {
-            Skip(SkipOption.SkipForward);
+            Skip(SkipOption.Forward);
         }
 
         private void QueueMedia_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
@@ -362,15 +383,52 @@ namespace MediaPlayerApp
 
         private void ButtonSavePlaylist_Click(object sender, RoutedEventArgs e)
         {
+            if (_currentPlaylist is not null)
+            {
+                var playlistPath = Path.Combine(Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName,
+                                            "Playlists", _currentPlaylist + ".txt");
+                using StreamWriter writer = File.CreateText(playlistPath);
+                foreach (var path in _queue)
+                {
+                    writer.WriteLine(path);
+                }
+                return;
+            }
+
             var dialog = new SavePlaylistDialog();
             dialog.ShowDialog();
             if (dialog.DialogResult == true)
             {
                 var playlistPath = dialog.PlaylistPath;
                 using StreamWriter writer = File.CreateText(playlistPath);
-                foreach (var path in _currentPlaylist)
+                foreach (var path in _queue)
                 {
                     writer.WriteLine(path);
+                }
+            }
+        }
+
+        private void ButtonRemoveItem_Click(object sender, RoutedEventArgs e)
+        {
+            var index = QueueMedia.SelectedIndex;
+
+            _queue.RemoveAt(index);
+        }
+
+        private void ButtoAddToQueue_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new OpenFileDialog
+            {
+                Filter = "Video files (*.mp4, *.wmv, *.avi)|*.mp4;*.wmv;*.avi|Audio files (*.mp3, *.wav)|*.mp3;*.wav",
+                InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyVideos),
+                Multiselect = true
+            };
+
+            if (dialog.ShowDialog() == true)
+            {
+                foreach (var name in dialog.FileNames)
+                {
+                    _queue.Add(name);
                 }
             }
         }
